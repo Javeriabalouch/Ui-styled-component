f90026f562fa2c394afd0efda8dcc8e3cc455628
javeria balouch <javeriabalouch@joyngroup.us>
2023-07-29 20:21:49 +0500

    #comment add featured product component folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/NavBar";
-import MainContent from "./Components/MainContent.js";
-
+import HeroBanner from "./Components/HeroBanner.js";
 
 function App() {
   return (
     <div className="App">
-  <NavBar/>
-  <MainContent/>
+      <NavBar />
+      <HeroBanner />
     </div>
   );
 }
